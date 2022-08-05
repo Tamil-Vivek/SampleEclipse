6359ef261f8879c0709fc0bc90b63f19869931bd
Tamil-Vivek <tamilarasiece97@gmail.com>
2022-08-05 12:13:08 +0530

    New project in Eclipse

---
+++ src/org/git/SampleForGit.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class SampleForGit {
+
+	public static void main(String[] args) {
+
+		System.out.println("Tamil");
+		System.out.println("Vivek");
+		System.out.println("krisha");
+	}
+
+}
